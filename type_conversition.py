c1afd21021ff99e7b382d372ea97109ab6554168
syed umer <umershakir987@gmail.com>
2026-02-12 11:30:44 +0500

    type converrsion str to float

--- type_conversition.py
+++ type_conversition.py
@@ -11,3 +11,8 @@ print(type(b))
 b=int(b)
 print(type(b))
 #str to float
+print("::::::::::::::::::::::::::::::::::::::::::::::str to float::::::::::::::::::::::::::::::::::::::::::::::::::")
+c= 10
+print(type(c))
+c= float(c)
+print(type(c))
