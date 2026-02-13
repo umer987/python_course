d97b777233bb34bf2c20f691b624565e42841eb8
syed umer <umershakir987@gmail.com>
2026-02-14 00:59:29 +0500

    IF ELSE LESSON ELIF

--- if_else.py
+++ if_else.py
@@ -7,4 +7,12 @@ if age > 18:
 else:
     print("YOU ARE NOT ELIGBLE FOR VOTING")
 
-
+print("::::::::::::::::::::::::::::::::::::ELSE ELIF ELSE::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+age = int(input("ENTER YOUR AGE"))
+if age >= 18:
+    print("YOU ARE ELIGIBLE FOR VOTING")
+elif age >= 15 or age <18:
+    print("VOTE PRACTICE YOU CAN VOTE BUT YOUR VOTE WILL NOT CONSIDER SERIOUS")
+else:
+    print("YOU ARE NOT ELIGBLE FOR VOTING")
+    
