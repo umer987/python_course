3e0ac0dee8c6d39ccee8003638dd8c3f63eb1abb
syed umer <umershakir987@gmail.com>
2026-02-14 01:20:30 +0500

    IF ELSE LESSON ELIF other excersises

--- else_if_practice.py
+++ else_if_practice.py
@@ -8,9 +8,25 @@ elif b > a:
 else:
     print("BOTH NUMBER ARE EQUAL")"""
 #accept the gender as a cahracter and give greeting like GM sir , GM maam
-gender = input("ENTER YOUR GENDER (M/F)")
+"""gender = input("ENTER YOUR GENDER (M/F)")
 if gender == "m":
     print("GOOD MORNING SIR!")
 
 else:
-    print("GOOD MORNING MAAM!") 
+    print("GOOD MORNING MAAM!") """
+#odd even number excersis
+"""print("::::::::::::::::::::::::::::::::::::::::::EVEN AND ODD NUMBER CHECKER::::::::::::::::::::::::::::::::::::::::::::")
+num = int(input("ENTER THE INTERGER"))
+if num % 2 == 0:
+    print("INTEGER IS EVEN")
+else:
+    print("INTEGER IS ODD")"""
+#VOTING SYSTEM BUT WITH HOW MANY YEAR AFTER YOU CAN VOTE
+"""print("::::::::::::::::::::::::::::::::::::::::::::::::VOTING SYSTEM::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+age = int(input("ENTER YOUR AGE"))
+if age > 18:
+    print("YOU ARE ELIGIBLE FOR VOTING")
+elif age< 18:
+    print("YOU CAN VOTE AFTER ", 18 -age," YEARS")
+else:
+    print("YOU ARE NOT ELIGBLE FOR VOTING")"""
