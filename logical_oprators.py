008698dda0a1144b9e00fc4faed1eba177ab61a9
syed umer <umershakir987@gmail.com>
2026-02-14 00:14:47 +0500

    more logical  oprators lessons and code for OR

--- logical_oprators.py
+++ logical_oprators.py
@@ -1,5 +1,12 @@
-#and oprator says if all results are true so i will give true val else if one value become false the result of all values will become false
+#AND oprator says if all results are true so i will give true val else if one value become false the result of all values will become false
+print(":::::::::::::::::::::::::::::::::::::::::AND:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
 print(12>10 and 10 == 10 )
 print(12>10 and 10 == 10 and 30 != 40 )
 #false only one value return false whole logic become false
 print(12>10 and 10 == 10 and 30 != 40  and 40 == 4)
+#OR oprator says if all results are false so i will give false val else if one value become true the result of all values will become true
+print(":::::::::::::::::::::::::::::::::::::::::OR:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+print(10 > 20 or 20<10 )
+#true only one value return true whole logic become true
+print(10 > 20 or 20<10 or 45 != 45 or 10 == 10)
+
