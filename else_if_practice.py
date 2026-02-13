e8f93881445846575a7f78e2758be99cc0e4fc68
syed umer <umershakir987@gmail.com>
2026-02-14 01:09:36 +0500

    IF ELSE LESSON ELIF other excersises

---
+++ else_if_practice.py
@@ -0,0 +1,16 @@
+#accept two numbers and point the gratest
+"""a = int(input("ENTER NUMBER ONE"))
+b = int(input("ENTER NUMBER TWO"))
+if a > b:
+    print(f"FIRST NUMBER IS GRATEST {a}")
+elif b > a:
+    print(f"SECOND NUMBER IS GRATEST {b}")
+else:
+    print("BOTH NUMBER ARE EQUAL")"""
+#accept the gender as a cahracter and give greeting like GM sir , GM maam
+gender = input("ENTER YOUR GENDER (M/F)")
+if gender == "m":
+    print("GOOD MORNING SIR!")
+
+else:
+    print("GOOD MORNING MAAM!") 
