acbfac988677e16e183816eb47f6458199263840
syed umer <umershakir987@gmail.com>
2026-02-14 00:51:10 +0500

    IF ELSE LESSON

---
+++ if_else.py
@@ -0,0 +1,10 @@
+#voting system by using IF-ELSE
+print("::::::::::::::::::::::::::::::::::::::::::::::::VOTING SYSTEM::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+age = int(input("ENTER YOUR AGE"))
+if age > 18:
+    print("YOU ARE ELIGIBLE FOR VOTING")
+
+else:
+    print("YOU ARE NOT ELIGBLE FOR VOTING")
+
+
