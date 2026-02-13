fe117a13ea1dc422506272bf88c8046455d1c3b1
syed umer <umershakir987@gmail.com>
2026-02-14 00:02:33 +0500

    logical  oprators lessons and code

--- logical_oprators.py
+++ logical_oprators.py
@@ -1,1 +1,3 @@
 #and oprator says if all results are true so i will give true val else if one value become false the result of all values will become false
+print(12>10 and 10 == 10 )
+print(12>10 and 10 == 10 and 30 != 40 )
