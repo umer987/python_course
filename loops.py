ad96078382bd62e10b54933425ac66adda4e1ef0
syed umer <umershakir987@gmail.com>
2026-02-15 00:02:12 +0500

    LOOPS LESSON AND SIMPLE TABLE PROGRAM

--- loops.py
+++ loops.py
@@ -1,5 +1,10 @@
 """a = range(0 , 20 , 1)
 for i in a:
     print(i)"""
-for i in range(1 , 20 ,1):
-    print(i)
+"""for i in range(1 , 20 ,2):
+    print(i)"""
+
+print("::::::::::::::::::::::::::::::::::::::::::::::::SIMPLE TABLE PROGRAM:::::::::::::::::::::::::::::::::::::::::::::")
+a= int(input("ENTER NUMBER FOR TABLE"))
+for i in range(1 ,11, 1):
+    print(f" {a} X {i} = {a*i}" )
