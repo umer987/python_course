7e17209ffdb75c4f3057dfa57f0004452bf75e55
syed umer <umershakir987@gmail.com>
2026-02-17 00:22:06 +0500

    ADDITION TO N

--- loops_to_paste.py
+++ loops_to_paste.py
@@ -1,4 +1,10 @@
-print("::::::::::::::::::::::::::::::::::TABLE PROGRRAM:::::::::::::::::::::::::::::::::::")
+"""print("::::::::::::::::::::::::::::::::::TABLE PROGRRAM:::::::::::::::::::::::::::::::::::")
 number = int(input("ENTER NUMBER FOR TABLE"))
 for i in range(1,11,1):
-    print(f"{number} X {i} = {number*i}")
+    print(f"{number} X {i} = {number*i}")"""
+
+#sum up to n numbers
+number = int(input("ENTER NUMBER TO SUM"))
+for i in range(1,number,1):
+    print(i)
+    print("ADDITON")
