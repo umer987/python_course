f5b5cd57c03c25d72529ebb3944320add8793d06
syed umer <umershakir987@gmail.com>
2026-02-16 23:00:24 +0500

    LOOPS EXCERSICES LOOP 2

--- loops_practice.py
+++ loops_practice.py
@@ -3,7 +3,13 @@ j = int(input("ENTER THE NUMBER OF TIMES YOU WANT TO PRINT HELLO WORLD"))
 for i in range(1,j+1,1):
     print("HELLO WORLD")"""
 
-#print the natural number upto n
+"""#print the natural number upto n
 j = int(input("ENTER THE NUMBER TO PRINT"))
 for i in range(1,j+1,1):
+    print(i)"""
+
+#(ENHANCED) print the natural number upto n
+l = int(input("ENTER THE NUMBER TO START"))
+j = int(input("ENTER THE NUMBER TO STOP"))
+for i in range(l,j+1,1):
     print(i)
