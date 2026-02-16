504800fa8cc2ca2153a402bfa0c5618e424b6c50
syed umer <umershakir987@gmail.com>
2026-02-17 00:11:42 +0500

    LOOPS EXCERSICES LOOP 5 UPDATED WITH NEW SOME COMPLETE CODE

--- loops_to_paste.py
+++ loops_to_paste.py
@@ -1,2 +1,4 @@
 print("::::::::::::::::::::::::::::::::::TABLE PROGRRAM:::::::::::::::::::::::::::::::::::")
 number = int(input("ENTER NUMBER FOR TABLE"))
+for i in range(1,11,1):
+    print(f"{number} X {i} = {number*i}")
