3b3a47773139858db64e3f1ec9f7095ab6c5dcca
syed umer <umershakir987@gmail.com>
2026-02-16 22:53:43 +0500

    LOOPS EXCERSICES LOOP 1

--- loops_practice.py
+++ loops_practice.py
@@ -1,3 +1,4 @@
 #accept an integer and print n number of times hello world
-i = int(input("ENTER THE NUMBER OF TIMES YOU WANT TO PRINT HELLO WORLD"))
-for i in
+j = int(input("ENTER THE NUMBER OF TIMES YOU WANT TO PRINT HELLO WORLD"))
+for i in range(1,j+1,1):
+    print("HELLO WORLD")
