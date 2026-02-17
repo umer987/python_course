9a6c62e67a41b6446e58bdfe6c9f970316578053
syed umer <umershakir987@gmail.com>
2026-02-17 23:19:10 +0500

    FACTORIAL INCOMPLETE

--- loops_to_paste.py
+++ loops_to_paste.py
@@ -3,10 +3,18 @@ number = int(input("ENTER NUMBER FOR TABLE"))
 for i in range(1,11,1):
     print(f"{number} X {i} = {number*i}")"""
 
-#sum up to n numbers
+"""#sum up to n numbers
 number = int(input("ENTER NUMBER TO SUM"))
 add=0
 for i in range(1,number+1,1):
     print(i)
     add += i
-    print("ADDITON ",add)
+    print("ADDITON ",add)"""
+#factorial program
+number = int(input("ENTER NUMBER TO FACRORIAL"))
+factorial =0
+for i in range(number,0,-1):
+    print(i)
+    factorial+=i * i-1
+    
+print(factorial)
