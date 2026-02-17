1dcb856cef758877d6152c6c9a1eeb1c633f3851
syed umer <umershakir987@gmail.com>
2026-02-18 00:52:12 +0500

    adding code more

--- LOOPS_FINAL.py
+++ LOOPS_FINAL.py
@@ -19,3 +19,34 @@ l = int(input("ENTER THE NUMBER TO START"))
 for i in range(l ,0,-1):
     print(i)
 """
+"""print("::::::::::::::::::::::::::::::::::TABLE PROGRRAM:::::::::::::::::::::::::::::::::::")
+number = int(input("ENTER NUMBER FOR TABLE"))
+for i in range(1,11,1):
+    print(f"{number} X {i} = {number*i}")"""
+
+"""#sum up to n numbers
+number = int(input("ENTER NUMBER TO SUM"))
+add=0
+for i in range(1,number+1,1):
+    print(i)
+    add += i
+    print("ADDITON ",add)"""
+#factorial program
+"""number = int(input("ENTER NUMBER TO FACRORIAL"))
+fact=1
+for i in range(1,number+1,1):
+  fact*=i
+print(fact)"""
+
+
+"""#if number i sperfect or not
+number = int(input("ENTER NUMBER TO CHECK IF IT IS PERFECT OR NOT"))
+factors = 0 
+for i in range(1,number,1):
+    if number % i == 0:
+        factors+=i
+
+if number == factors:
+    print("GIVEN NUMBER IS PERFECT")
+else:
+    print("GIVEN NUMBER IS NOT PERFECT")"""
