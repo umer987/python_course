ffa5b6b88c5829a131f859963556a88ba16dc801
syed umer <umershakir987@gmail.com>
2026-02-18 00:51:30 +0500

    adding code

---
+++ LOOPS_FINAL.py
@@ -0,0 +1,21 @@
+"""#accept an integer and print N number of times hello world
+j = int(input("ENTER THE NUMBER OF TIMES YOU WANT TO PRINT HELLO WORLD"))
+for i in range(1,j+1,1):
+    print("HELLO WORLD")"""
+
+"""#print the natural number upto N
+j = int(input("ENTER THE NUMBER TO PRINT"))
+for i in range(1,j+1,1):
+    print(i)"""
+
+"""#(ENHANCED) print the natural number upto N
+l = int(input("ENTER THE NUMBER TO START"))
+j = int(input("ENTER THE NUMBER TO STOP"))
+for i in range(l,j+1,1):
+    print(i)"""
+
+"""#print negative number to N
+l = int(input("ENTER THE NUMBER TO START"))
+for i in range(l ,0,-1):
+    print(i)
+"""
