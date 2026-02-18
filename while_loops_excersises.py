c7088e5bfabc362a592de9cbc25db554b3a51afe
syed umer <umershakir987@gmail.com>
2026-02-19 01:35:46 +0500

    ADDING WHILE LOOP EXCERSISE ONE REVERSE ORDER COMPLETE

--- while_loops_excersises.py
+++ while_loops_excersises.py
@@ -1,7 +1,30 @@
-#seprate each digit of a given number and and print it on a new line
+"""#seprate each digit of a given number and and print it on a new line
 number = input("ENTER THE NUMBER")
 leno = 0
 while leno != len(number):
     print(number[leno])
     leno += 1
-print("string to int" , int(number)+1)
+print("string to int" , int(number)+1)"""
+
+#seprate each digit of a given number and and print it on a new line reverse order
+number = input("ENTER THE NUMBER")
+leno = len(number)
+while leno != 0:
+    print(number[leno-1])
+    leno -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+#accept the number and print its reverse
+
+
