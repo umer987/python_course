b2700204e357ee0092bf5369555169bee95722d5
syed umer <umershakir987@gmail.com>
2026-02-19 01:25:33 +0500

    ADDING WHILE LOOP EXCERSISE ONE

---
+++ while_loops_excersises.py
@@ -0,0 +1,7 @@
+#seprate each digit of a given number and and print it on a new line
+number = input("ENTER THE NUMBER")
+leno = 0
+while leno != len(number):
+    print(number[leno])
+    leno += 1
+
