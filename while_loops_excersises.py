a1526d99875e52cb7d7d626d34498571f60d3def
syed umer <umershakir987@gmail.com>
2026-02-19 01:45:36 +0500

    ADDING WHILE LOOP EXCERSISE ACCEPT NUMBER AND PRINT REVERSE

--- while_loops_excersises.py
+++ while_loops_excersises.py
@@ -6,25 +6,15 @@ while leno != len(number):
     leno += 1
 print("string to int" , int(number)+1)"""
 
-#seprate each digit of a given number and and print it on a new line reverse order
+"""#seprate each digit of a given number and and print it on a new line reverse order
 number = input("ENTER THE NUMBER")
 leno = len(number)
 while leno != 0:
     print(number[leno-1])
-    leno -= 1
-
-
-
-
-
-
-
-
-
-
-
-
+    leno -= 1"""
 
 #accept the number and print its reverse
+number = input("ENTER THE NUMBER")
+
 
 
