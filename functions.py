568ab2a4d562ff030b9b9fe762310fbe456893ea
syed umer <umershakir987@gmail.com>
2026-02-22 00:26:11 +0500

    adding parameters functions lessons

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
+#simple function
 def add_2_number():
     print("function called")
-
-
 add_2_number()
+
+#parameterized function
+number_1 = int(input("ENTER NUMBER 1:- "))
+number_2 = int(input("ENTER NUMBER 2:- "))
+
+def add(a,b):
+    print("THE ADDITION OF 2 NUMBERS IS ", a+b)
+
+add(number_1,number_2)
