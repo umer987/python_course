74def9392ffdd81c4555d33e25892717ee5dd2ca
syed umer <umershakir987@gmail.com>
2026-02-22 01:02:59 +0500

    adding parameters functions palandrom function

--- functions.py
+++ functions.py
@@ -10,3 +10,26 @@ def add(a,b):
     print("THE ADDITION OF 2 NUMBERS IS ", a+b)
 add(number_1,number_2)"""
 
+"""stng =input("ENTER AN STRING TO CHECK PALANDROM")
+
+def chck_plndro(a):
+    lng = len(a)
+    print(lng)
+    new =""
+    for i in range(lng,1-1,-1):
+        print(i)
+        new+=a[i]
+        if new == a:
+            print("STRING IS PALANDROM")
+        else:
+            print("STRING IS NOT PALANDROM")
+
+chck_plndro(stng)"""
+
+
+stng =input("ENTER AN STRING TO CHECK PALANDROM")
+lng = len(stng)
+new=""
+for i in range(lng,1-1,-1):
+    print(i)
+    new+=
