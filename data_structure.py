f433810941b3355b61ed6fad7b876587f78b44a7
syed umer <umershakir987@gmail.com>
2026-02-23 06:47:04 +0500

    adding data structure method two to traverse a lists

--- data_structure.py
+++ data_structure.py
@@ -1,5 +1,8 @@
 #list in python
 a =[21,44,421,664,21]
 #method 1 traversing the list
-for i in range(len(a)):
-    print(a[i])
+#for i in range(len(a)):
+   # print(a[i])
+#method 2 of traversing a list
+for i in a:
+    print(i)
