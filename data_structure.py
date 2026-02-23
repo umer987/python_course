2af48a6fccb1fc185644502bb3a0ba7b7c8b706a
syed umer <umershakir987@gmail.com>
2026-02-23 22:18:12 +0500

    list clear  method complete

--- data_structure.py
+++ data_structure.py
@@ -21,11 +21,13 @@ a =[21,44,421,664,21]
 # a.insert(3,422)
 # print(a)
 #extend method functaniolaties are add multiple elements in the lis but within in syntax([1,2,3,4])
-b =[21,44,421,664,21]
+b =[21,44,421,664,281]
 #b.extend([69,72,12,35])
 #remove method functaniolites are it remove desire element from list
 # b.remove(44)
 # b.remove(21)
 # it removes whole list
 # b.clear()
+# reverse method reverse whole lists
+# b.reverse()
 print(b)
