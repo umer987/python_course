e993bf97fb541a8cdc1e29da3129d9341d10f603
syed umer <umershakir987@gmail.com>
2026-02-25 22:21:46 +0500

    find mean from an list incomplete

--- list_excersises.py
+++ list_excersises.py
@@ -1,12 +1,16 @@
 #question is find the positive and negative element in list
-l = [-1,2,-9,5,-12,6,-22]
-n=[]
-p=[]
-for i in range(0,len(l),1):
-    if l[i]<0:
-        n.append(l[i])
-    else:
-        p.append(l[i])
-print("NEGATIVE ELEMENTS ARE:- ",n)
-print("POSITVE ELEMENTS ARE:- ", p)
+# l = [-1,2,-9,5,-12,6,-22]
+# n=[]
+# p=[]
+# for i in range(0,len(l),1):
+#     if l[i]<0:
+#         n.append(l[i])
+#     else:
+#         p.append(l[i])
+# print("NEGATIVE ELEMENTS ARE:- ",n)
+# print("POSITVE ELEMENTS ARE:- ", p)
 
+
+#solve mean for an givin list
+l=[20,22,24,26,28,30]
+total=0
