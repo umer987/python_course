ba076e95471f27e48d872f35b1ba11225877a7e0
syed umer <umershakir987@gmail.com>
2026-02-26 22:09:37 +0500

    methodes of tuple

--- tuple.py
+++ tuple.py
@@ -5,6 +5,8 @@
 
 
 #traverse tuple thorugh loop
-t =(1,2,3,4,5)
-for i in range(0,len(t),1):
-    print(t[i])
+# t =(1,2,3,4,5)
+# for i in range(0,len(t),1):
+#     print(t[i])
+
+#methodes of tuple
