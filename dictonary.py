3eb275d6dadd1a9ca2628c484c3385e6634cd622
syed umer <umershakir987@gmail.com>
2026-02-27 22:50:16 +0500

    adding dictonary student record

--- dictonary.py
+++ dictonary.py
@@ -1,3 +1,6 @@
-#dictonary simple syntax
-student = {1:"umer",2:"b",3:"c"}
-print(student)
+#dictonary simple syntax it is like table in sql
+# student = {1:"umer",2:"b",3:"c"}
+# print(student)
+
+#make student record 
+student={"name":"umer", "father_name":"muhammad shakir", "age":"21"}
