652f60238328c479ad666b9d63f12f6718f573d3
syed umer <umershakir987@gmail.com>
2026-02-27 22:58:46 +0500

    traverse dictonary through loop

--- dictonary.py
+++ dictonary.py
@@ -3,5 +3,11 @@
 # print(student)
 
 #make student record 
-student={"name":"umer", "father_name":"muhammad shakir", "age":"21"}
-print(student["name"])
+# student={"name":"umer", "father_name":"muhammad shakir", "age":"21"}
+# print(student["name"])
+
+#traverse dictonary through loop
+student = {1:"umer",2:"b",3:"c"}
+
+for i in range(1,len(student)+1,1):
+    print(student[i])
