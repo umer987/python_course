3eba4bfed7fb77185919d0ce495c1f5240cc495a
syed umer <umershakir987@gmail.com>
2026-02-28 22:36:08 +0500

    traverse through loop advance

--- dictonary.py
+++ dictonary.py
@@ -22,7 +22,7 @@
 
 #traverse through loop advance
 
-d ={10:1,20:2,30:3,40:4}
-for i in d:
-    #print(i) #it will only print key but we want values
-    print(d[i])
+# d ={10:1,20:2,30:3,40:4}
+# for i in d:
+#     #print(i) #it will only print key but we want values
+#     print(d[i])
