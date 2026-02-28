0723674f94063558cdf7cf8c48056c85df92a908
syed umer <umershakir987@gmail.com>
2026-02-28 22:39:36 +0500

    simple calculator

--- trash.py
+++ trash.py
@@ -1,22 +1,55 @@
 #number guessing game
-import random
+# import random
 
-number = random.randint(1, 100)
-attempts = 0
+# number = random.randint(1, 100)
+# attempts = 0
 
-print("Guess the number between 1 and 100!")
+# print("Guess the number between 1 and 100!")
 
-while True:
-    try:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
+# while True:
+#     try:
+#         guess = int(input("Enter your guess: "))
+#         attempts += 1
         
-        if guess < number:
-            print("Too low!")
-        elif guess > number:
-            print("Too high!")
+#         if guess < number:
+#             print("Too low!")
+#         elif guess > number:
+#             print("Too high!")
+#         else:
+#             print(f"Correct! You guessed it in {attempts} attempts!")
+#             break
+#     except ValueError:
+#         print("Please enter a valid number!")
+
+
+#simple calculator
+def calculator():
+    print("Simple Calculator")
+    print("Operations: +, -, *, /")
+    
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        operation = input("Enter operation (+, -, *, /): ")
+        
+        if operation == '+':
+            result = num1 + num2
+        elif operation == '-':
+            result = num1 - num2
+        elif operation == '*':
+            result = num1 * num2
+        elif operation == '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                print("Error: Division by zero!")
+                return
         else:
-            print(f"Correct! You guessed it in {attempts} attempts!")
-            break
+            print("Invalid operation!")
+            return
+            
+        print(f"Result: {num1} {operation} {num2} = {result}")
     except ValueError:
-        print("Please enter a valid number!")
+        print("Please enter valid numbers!")
+
+calculator()
