835e54bb3ace43d46b5030cdf60968f1ed08482b
syed umer <umershakir987@gmail.com>
2026-03-01 22:23:39 +0500

    Count the frequency of each element

--- dictonary.py
+++ dictonary.py
@@ -43,11 +43,11 @@
 # print(sum)
 
 #Count the frequency of each element
-d ={1:1,2:2,3:3,4:4,5:4,6:4,7:4,8:4,9:4,10:2}
-number = int(input("ENTER THE NUMBER"))
-count =0
-for i in range(1,len(d)+1,1):
-    if d[i] == number:
-        count +=1
+# d ={1:1,2:2,3:3,4:4,5:4,6:4,7:4,8:4,9:4,10:2}
+# number = int(input("ENTER THE NUMBER"))
+# count =0
+# for i in range(1,len(d)+1,1):
+#     if d[i] == number:
+#         count +=1
 
-print(f"the frequency of the {number} is {count}")
+# print(f"the frequency of the {number} is {count}")
