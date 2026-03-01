6eebdb03571f4a4d00271a19a5181ba0f5e16ede
syed umer <umershakir987@gmail.com>
2026-03-01 22:32:18 +0500

    Write a Python program to combine two dictionary by adding values for common keys

--- dictonary.py
+++ dictonary.py
@@ -55,3 +55,4 @@
 #Write a Python program to combine two dictionary by adding  values for common keys
 d ={1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10}
 c ={1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10}
+merged =  d | c
