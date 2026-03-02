525dc93422bf7e51d5fa7678a16906dde2852fea
syed umer <umershakir987@gmail.com>
2026-03-02 22:01:09 +0500

    handle exception as error (err) try-except-else

--- exception_handling.py
+++ exception_handling.py
@@ -7,11 +7,14 @@
 
 # print("DONE")
 
-#handle exception as error (err)
-number =  int(input("ENTER THE NMBER TO DIVIDE"))
-try:
-    print(10/ number)
-except Exception as err:
-    print(f"ERROR {err}")
+#handle exception as error (err) try-except
+# number =  int(input("ENTER THE NMBER TO DIVIDE"))
+# try:
+#     print(10/ number)
+# except Exception as err:
+#     print(f"ERROR {err}")
 
-print("DONE")
+# print("DONE")
+
+#handle exception as error (err) try-except-else if except run so else wont and else run except wont run
+number =  int(input("ENTER THE NMBER TO DIVIDE"))
