7a57491570b1820b40025cf6a45ea8512523604c
syed umer <umershakir987@gmail.com>
2026-03-09 00:12:41 +0500

    adding new fille some code

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,6 @@
 #to read file and open file
-p =open(r"C:\Users\UMER QURESHI\Desktop\abc.txt")
-print(p.read())
+# p =open(r"C:\Users\UMER QURESHI\Desktop\abc.txt")
+# print(p.read())
+
+#create file
+r =open('azc.txt','w')
