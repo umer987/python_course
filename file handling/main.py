4a721fcf3e8ba644c196bca689feab6c4e5c7a2b
syed umer <umershakir987@gmail.com>
2026-03-09 01:08:53 +0500

    file handling project complete

--- file handling/main.py
+++ file handling/main.py
@@ -4,23 +4,38 @@ print("PRESS 2 FOR READ A FILE")
 print("PRESS 3 FOR UPDATE A FILE")
 print("PRESS 4 FOR DELETE A FILE")
 check = int(input("PLEASE ENTER THE NUMBER"))
-if check == 1:
+
+def create():
     filename = input("ENTER FILE NAME")
     filecontent= input("ENTER FILE CONTENT")
     f =open(f"{filename}.txt",'w') 
     f.write(filecontent)
     f.close()
-elif check == 2:
+
+def read():
     filename = input("ENTER FILE NAME TO READ")
     f = open(rf"{filename}.txt")
     print(f.read())
-elif check == 3:
+
+def update():
     filename = input("ENTER FILE NAME")
     filecontent= input("ENTER FILE CONTENT")
     f =open(f"{filename}.txt",'a') 
     f.write(filecontent)
     f.close()
+
+def delete():
+    filename = input("ENTER FILE NAME")
+    os.remove(f"{filename}.txt")
+    print(f"File {filename} deleted successfully")
+
+if check == 1:
+    create()
+elif check == 2:
+    read()
+elif check == 3:
+    update()
 elif check == 4:
-     filename = input("ENTER FILE NAME")
-     os.remove(f"{filename}.txt")
-     print(f"File {filename} deleted successfully")
+    delete()
+elif check >=4:
+    print("YOU ENTER THE WRONG NUMBER CHOICE SHOULD BE 1,2,3,4")
