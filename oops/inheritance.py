29029655aaaadeb798cd168b8084eb4f2e5eee0b
syed umer <umershakir987@gmail.com>
2026-03-10 01:54:18 +0500

    adding inheritance file simple code

---
+++ oops/inheritance.py
@@ -0,0 +1,10 @@
+class FactoryKHI:
+    a ="this is KHI FACTORY ATTR"
+    def fun(self):
+        print("this is KHI FACTORY DEF")
+
+class FactoryISB(FactoryKHI):
+    pass
+obj = FactoryISB()
+print(obj.a)
+obj.fun()
