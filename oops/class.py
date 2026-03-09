05f943ee73486cce67f735ee5a51279a03ac5b20
syed umer <umershakir987@gmail.com>
2026-03-10 00:39:45 +0500

    starting oops lessons and adding class file code objects

--- oops/class.py
+++ oops/class.py
@@ -6,5 +6,9 @@ class Factory:
 
     print("HOW ARE YOU")
 
-print(Factory().a)
-Factory().hello()
+
+
+#talk about object objects is also blueprint of class means obj = factory() all powers of factory class become in obj
+# obj = Factory()
+# print(obj.a)
+# obj.hello() 
