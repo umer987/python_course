bf59ee4708d17918147e8054b8b7758979b48a16
syed umer <umershakir987@gmail.com>
2026-03-13 00:37:07 +0500

    #starting encapsulation lessons complete

--- oops/encapsulation.py
+++ oops/encapsulation.py
@@ -18,19 +18,19 @@
 
 
 #only privete access modifier works in python and which is __ double underscore
-class Factory:
-    __a = "KARACHI"
+# class Factory:
+#     __a = "KARACHI"
 
-    def __show(self):
-        print("hello umer shakir")
+#     def __show(self):
+#         print("hello umer shakir")
 
-class Factory2(Factory):
-    b = "ISLAMABAD"
+# class Factory2(Factory):
+#     b = "ISLAMABAD"
 
-    def show2(self):
-        print(super().__a)
+#     def show2(self):
+#         print(super().__a)
 
 
-obj = Factory2()
+# obj = Factory2()
 
-obj.show2()
+# obj.show2()
