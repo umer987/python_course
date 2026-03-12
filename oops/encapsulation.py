5485458bc9b8238f5551fecbbb0526c4ea37c97d
syed umer <umershakir987@gmail.com>
2026-03-13 00:30:07 +0500

    #starting encapsulation lesson

--- oops/encapsulation.py
+++ oops/encapsulation.py
@@ -1,17 +1,23 @@
 #starting encapsulation _single underscor isnot work in python python not support protected acces modifier
+# class Factory:
+#     _a = "KARACHI"
+
+#     def show(self):
+#         print("hello umer shakir")
+
+# class Factory2(Factory):
+#     b = "ISLAMABAD"
+
+#     def show2(self):
+#         print(super()._a)
+
+
+# obj = Factory2()
+
+# obj.show2()
+
+
+#only privete access modifier works in python and which is __ double underscore
 class Factory:
-    _a = "KARACHI"
+    __a = "KARACHI"
 
-    def show(self):
-        print("hello umer shakir")
-
-class Factory2(Factory):
-    b = "ISLAMABAD"
-
-    def show2(self):
-        print(super()._a)
-
-
-obj = Factory2()
-
-obj.show2()
