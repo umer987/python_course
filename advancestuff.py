f396ed696c13fa94c9e9d120850573d8a564a2f9
syed umer <umershakir987@gmail.com>
2026-03-14 00:01:16 +0500

    #starting advance stuff

---
+++ advancestuff.py
@@ -0,0 +1,49 @@
+#decotrators
+# def say_hello():
+#     print("Hello")
+
+# def my_decorator(func):
+#     def wrapper():
+#         print("Before function runs")
+#         func()
+#         print("After function runs")
+#     return wrapper
+
+# @my_decorator
+# def say_hello():
+#     print("Hello")
+
+# say_hello()
+
+#addition decorator
+# def decorator(func):
+#     def wrapper(a,b):
+#         print("THE TOTAL ADDITION OF YOUR NUMBERS ARE:- ")
+#         func(a,b)
+#         print("THANK YOU")
+#     return wrapper
+    
+# @decorator
+# def add(a,b):
+#     print(a+b)
+
+# add(1,2)
+
+# def decorator(func):
+#     def wrapper(*args):
+#         print("THE TOTAL ADDITION OF YOUR NUMBERS ARE:- ")
+#         func(*args)
+#         print("THANK YOU")
+#     return wrapper
+    
+# @decorator
+# def add(*args):
+#     sum = 0
+#     for i in args:
+#         sum = sum + i
+#     print(sum)
+
+# add(1,3,7,2)
+
+
+
