8fd4a9be077a36730143c81f3b7ab390634e6b9b
Umer Shakir <159330908+umer987@users.noreply.github.com>
2026-07-19 18:41:22 +0500

    Update also_trashh.py

--- also_trashh.py
+++ also_trashh.py
@@ -1,7 +1,6 @@
 
         
-        print(f"\n🎾 You play with {self.name}...")
-        self.happiness = min(100, self.happiness + 25)
+             self.happiness = min(100, self.happiness + 25)
         self.energy = max(0, self.energy - 20)
         self.hunger = min(100, self.hunger + 15)
         print(f"{self.name}: That was fun! Let's play again!")
