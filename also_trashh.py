2f6f18d476b45cd5b4b6889e64e60929dc3a1969
Umer Shakir <159330908+umer987@users.noreply.github.com>
2026-08-20 21:01:58 +0500

    Update also_trashh.py

--- also_trashh.py
+++ also_trashh.py
@@ -8,8 +8,7 @@
 
 
     
-    vowels = 'aeiouAEIOU'
-    return sum(1 for char in text if char in vowels)
+    (1 for char in text if char in vowels)
 
 # ====== SECTION 3: DECORATORS ======
 def timer(func):
