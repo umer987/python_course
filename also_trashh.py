275c06806be8a7322604b6cd19fa302f51a29c9f
Umer Shakir <159330908+umer987@users.noreply.github.com>
2026-08-30 11:19:40 -0700

    Implement basic Express server with routes
    Added a simple Express server with two routes.

--- also_trashh.py
+++ also_trashh.py
@@ -1,4 +1,13 @@
+const express = require('express') //importing server
 
+const app = express() //making server calass instance
+app.get("/",(req,res)=>{    
+    res.send("HELLO WORLD")
+})
+app.get('/about',(req,res)=>{
+    res.send("HELLO FROM ABOUT")
+})
+app.listen(3000)
     print(f"Squares: {collections['squares']}")
     print(f"Word Count: {collections['word_count']}")
     
